6179557d8af935a7cf5fbaee26f361ee1f8be33a
Luis Diaz <luisdiazugena@gmail.com>
2016-09-17 17:09:19 +0200

    First code to put serial info into the screen

--- serial-monitor/serial-monitor.ino
+++ serial-monitor/serial-monitor.ino
@@ -17,7 +17,7 @@
 #define TFT_DC 9
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 boolean bootScreenStatus = true;
-
+int indexPos = 0; // Where to print the data
 boolean backlightStatus = true;
 
 int serialSpeed=9600;
@@ -51,6 +51,7 @@ void loadConfig(){
 void tftInit(){
 
   tft.begin();
+  tft.clearScreen();
 
   if(bootScreenStatus==true){
     tft.setCursor(16,0);
@@ -63,12 +64,70 @@ void tftInit(){
     tft.println("DrMaker.es");
     delay(750);
   }
+  tft.clearScreen();
+  tft.setTextColor(WHITE);
+  tft.setTextSize(2);
 }
 
 void setup(){
   loadConfig(); // must be first thing to do in the setup
   tftInit();
   firstTimeInit(); // Check if there is a custom config or the program should create it.
-  
+
   Serial.begin(serialSpeed);
+}
+
+void loop() {
+  if(Serial.available()>0){
+    printData(Serial.readString());
+  }
+}
+String str0="", str1="", str2="", str3="", str4="";
+int spacing = 10;
+void printData(String _str){
+  if(indexPos==4){
+  }
+  if(_str.length()>0){
+    if (indexPos == 0){
+      str0=_str;
+      tft.setCursor((5-indexPos)*spacing);
+      tft.println(_str);
+    }
+    if (indexPos == 1){
+      str1=_str;
+      tft.setCursor((5-indexPos)*spacing);
+      tft.println(_str);
+    }
+    if (indexPos == 2){
+      str2=_str;
+      tft.setCursor((5-indexPos)*spacing);
+      tft.println(_str);
+    }
+    if (indexPos == 3){
+      str3=_str;
+      tft.setCursor((5-indexPos)*spacing);
+      tft.println(_str);
+    }
+    if (indexPos == 4){
+      changeStrPos();
+      str4=_str;
+      tft.setCursor((5-indexPos)*spacing);
+      tft.println(_str);
+    }
+  }
+}
+void changeStrPos(){
+  str0=str1;
+  str1=str2;
+  str2=str3;
+  str3=str4;
+  str4="";
+  tft.setCursor(5*spacing);
+  tft.println(str0);
+  tft.setCursor(4*spacing);
+  tft.println(str1);
+  tft.setCursor(3*spacing);
+  tft.println(str2);
+  tft.setCursor(2*spacing);
+  tft.println(str3);
 }
