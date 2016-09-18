158cfae628ac214ee03aa368ab27531e4f7a3eba
Luis Diaz <luisdiazugena@gmail.com>
2016-09-18 18:28:28 +0200

    Revert "Fully rewrite of the code"
    This reverts commit aa2ef69f7040392d2d7015e90fc53925a89814e8.
    
    # Conflicts:
    #	serial-monitor/serial-monitor.ino

--- serial-monitor/serial-monitor.ino
+++ serial-monitor/serial-monitor.ino
@@ -1,133 +1,52 @@
-#include <EEPROM.h>
-#include <Adafruit_ILI9341.h>
+#include <SPI.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-//Colors init
-#define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
-#define GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
 
-//TFT init
-#define TFT_CS 10
-#define TFT_DC 9
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-boolean bootScreenStatus = true;
-int indexPos = 0; // Where to print the data
-boolean backlightStatus = true;
+#define okBtn 7
+#define moveBtn 8
+#define delay_time 750
 
-int serialSpeed=9600;
-//eeprom addresses
-#define firstTimeAddress 0
-#define serialSpeedAddress 1
-#define backlightAddress 2
-#define bootScreenAddress 3
+void setup() {
 
-void firstTimeInit(){
-  if (EEPROM.read(firstTimeAddress)==0){
-    EEPROM.write(serialSpeedAddress,serialSpeed);
-    EEPROM.write(backlightAddress,1); // 1 to turn on the backlight
-    EEPROM.write(bootScreenAddress,true); // 1 to turn on the bootScreen
+  Serial.begin(19200);
+  pinMode(okBtn,INPUT_PULLUP);
+  pinMode(moveBtn,INPUT_PULLUP);
 
-    EEPROM.write(firstTimeAddress,1);
-  }
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  showInitInfo();
 }
-
-void loadConfig(){
-  serialSpeed = EEPROM.read(serialSpeedAddress);
-  backlightStatus = EEPROM.read(backlightStatus);
-
-  if(EEPROM.read(bootScreenAddress)){
-    bootScreenStatus = true;
-  }else{
-    bootScreenStatus = false;
-  }
-}
-
-void tftInit(){
-
-  tft.begin();
-  tft.clearScreen();
-
-  if(bootScreenStatus==true){
-    tft.setCursor(16,0);
-    tft.setTextColor(WHITE);
-    tft.setTextSize(2);
-    tft.println("Hardware Serial Monitor");
-    tft.setCursor(16,20);
-    tft.setTextColor(RED);
-    tft.setTextSize(4);
-    tft.println("DrMaker.es");
-    delay(750);
-  }
-  tft.clearScreen();
-  tft.setTextColor(WHITE);
-  tft.setTextSize(2);
-}
-
-void setup(){
-  loadConfig(); // must be first thing to do in the setup
-  tftInit();
-  firstTimeInit(); // Check if there is a custom config or the program should create it.
-
-  Serial.begin(serialSpeed);
-}
-
+String str;
 void loop() {
-  if(Serial.available()>0){
-    printData(Serial.readString());
+  if (digitalRead(okBtn) == 1){
+    //showMenu();
   }
+  if (Serial.available() > 0) {
+    display.clearDisplay();
+    str = Serial.readString();
+    display.setCursor(0, 0);
+    display.println(str);
+    display.display();
+  }
+
 }
-String str0="", str1="", str2="", str3="", str4="";
-int spacing = 10;
-void printData(String _str){
-  if(indexPos==4){
-  }
-  if(_str.length()>0){
-    if (indexPos == 0){
-      str0=_str;
-      tft.setCursor((5-indexPos)*spacing);
-      tft.println(_str);
-    }
-    if (indexPos == 1){
-      str1=_str;
-      tft.setCursor((5-indexPos)*spacing);
-      tft.println(_str);
-    }
-    if (indexPos == 2){
-      str2=_str;
-      tft.setCursor((5-indexPos)*spacing);
-      tft.println(_str);
-    }
-    if (indexPos == 3){
-      str3=_str;
-      tft.setCursor((5-indexPos)*spacing);
-      tft.println(_str);
-    }
-    if (indexPos == 4){
-      changeStrPos();
-      str4=_str;
-      tft.setCursor((5-indexPos)*spacing);
-      tft.println(_str);
-    }
-  }
-}
-void changeStrPos(){
-  str0=str1;
-  str1=str2;
-  str2=str3;
-  str3=str4;
-  str4="";
-  tft.setCursor(5*spacing);
-  tft.println(str0);
-  tft.setCursor(4*spacing);
-  tft.println(str1);
-  tft.setCursor(3*spacing);
-  tft.println(str2);
-  tft.setCursor(2*spacing);
-  tft.println(str3);
+void showInitInfo() {
+  display.clearDisplay();
+  delay(10);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Serial");
+  display.setCursor(0, 17);
+  display.println("Monitor");
+  display.display();
+  delay(750);
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.println("Waiting...");
+  display.display();
 }
